c010f16fc132c16ab736e48e768158ab07a1c88f
trunghau810 <tthau2004@gmail.com>
2025-04-10 17:40:20 +0700

    Update models and admin site

--- clinicbooking/clinic/admin.py
+++ clinicbooking/clinic/admin.py
@@ -1,3 +1,113 @@
 from django.contrib import admin
+from clinic.models import (User, Doctor, Patient, HealthRecord, Schedule,
+                           Appointment, Review, Message,
+                           Payment, TestResult, Notification)
+from django.utils.html import mark_safe
 
-# Register your models here.
+
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'first_name', 'last_name', 'email', 'number_phone']
+    search_fields = ['username', 'first_name', 'last_name']
+    list_filter = ['id']
+    readonly_fields = ['avatar_view']
+
+    # Gỡ bỏ các field không mong muốn, chỉ hiển thị các fied cần thiết
+    fieldsets = (
+        ("Thông tin tài khoản", {'fields': ('username', 'password')}),
+        ('Thông tin cá nhân', {'fields': ('first_name', 'last_name', 'gender', 'number_phone', 'email')}),
+        ('Thông tin khác', {'fields': ('avatar', 'avatar_view', 'user_type')}),
+    )
+
+    def avatar_view(self, user):
+        return mark_safe(f"<img src='https://res.cloudinary.com/tthau2004/{user.avatar}' width=200 />")
+
+    def save_model(self, request, user, form, change):
+        if form.cleaned_data.get('password'):
+            raw_password = form.cleaned_data['password']
+            if not raw_password.startswith('pbkdf2_'):  # chỉ set nếu là mật khẩu mới
+                user.set_password(raw_password)
+
+            super().save_model(request, user, form, change)
+
+
+class MyDoctorAdmin(MyUserAdmin):
+    list_display = MyUserAdmin.list_display + ['license_number']
+    readonly_fields = ['avatar_view', 'license_view']
+    fieldsets = MyUserAdmin.fieldsets + (
+        ("Giấy phép hành nghề", {'fields': ('license_number', 'license_image', 'license_view', 'is_verified')}),
+        ("Nơi làm việc", {'fields': ('hospital', 'specialty')}),
+    )
+
+    def get_changeform_initial_data(self, request):
+        return {'user_type': 'Dr'}
+
+    def license_view(self, doctor):
+        return mark_safe(f"<img src='https://res.cloudinary.com/tthau2004/{doctor.license_image}' width=500 />")
+
+
+class MyPatientAdmin(MyUserAdmin):
+    fieldsets = (
+        ("Thông tin tài khoản", {'fields': ('username', 'password')}),
+        ('Thông tin cá nhân', {
+            'fields': ('first_name', 'last_name', 'gender', 'day_of_birth', 'number_phone', 'address', 'email')
+            # thêm ở đây
+        }),
+        ('Thông tin khác', {
+            'fields': ('avatar', 'avatar_view', 'user_type')
+        }),
+    )
+
+
+class MyAppointmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'disease_type', 'booked_at']
+
+
+class MyScheduleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'date', 'start_time', 'end_time', 'is_available', 'doctor_id']
+
+
+class MyPaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'formatted_amount', 'method', 'status', 'created_date', 'updated_date', 'appointment_id']
+
+    def formatted_amount(self, obj):
+        return f"{obj.amount:,.0f} ₫"
+
+
+class MyHealthRecordAdmin(admin.ModelAdmin):
+    list_display = ['id', 'id_patient', 'patient_username', 'medical_history', 'active', 'created_date', 'updated_date']
+
+    def id_patient(self, patient):
+        return patient.patient_id
+
+    def patient_username(self, patient):
+        first_name = patient.patient.first_name
+        last_name = patient.patient.last_name
+        return (f"{first_name} {last_name}")
+
+    id_patient.short_description = 'ID bệnh nhân'
+    patient_username.short_description = 'Tên tài khoản bệnh nhân'
+
+
+class MyTestReusultAdmin(admin.ModelAdmin):
+    list_display = ['id', 'health_record', 'test_name', 'created_date', 'updated_date']
+
+
+class ClinicAdminSite(admin.AdminSite):
+    site_header = 'Hệ thống quản trị đặt lịch khám sức khoẻ trực tuyến'
+    site_title = "Clinic Admin"
+    index_title = 'Trang quản trị'
+
+
+admin_site = ClinicAdminSite(name='myadmin')
+
+admin_site.register(User, MyUserAdmin)
+admin_site.register(Doctor, MyDoctorAdmin)
+admin_site.register(Patient, MyPatientAdmin)
+admin_site.register(HealthRecord, MyHealthRecordAdmin)
+admin_site.register(Schedule, MyScheduleAdmin)
+admin_site.register(TestResult, MyTestReusultAdmin)
+admin_site.register(Message)
+admin_site.register(Appointment, MyAppointmentAdmin)
+admin_site.register(Review)
+admin_site.register(Payment, MyPaymentAdmin)
+admin_site.register(Notification)
