6a10a7b7eb91cb8d81dbcd4ec106de94c1dd13d8
trunghau810 <tthau2004@gmail.com>
2025-04-23 17:18:46 +0700

    Update API healthrecord and testresult

--- clinicbooking/clinic/urls.py
+++ clinicbooking/clinic/urls.py
@@ -9,6 +9,8 @@ router.register('doctors', views.DoctorViewSet, basename='doctor')
 router.register('patients', views.PatientViewSet, basename='patient')
 router.register('appointments', views.AppointmentViewSet, basename='appointment')
 router.register('schedules', views.ScheduleViewSet, basename='schedule')
+router.register('healthrecords', views.HealthRecordViewSet, basename='healthrecord')
+router.register('testresults', views.TestResultViewSet, basename='testresult')
 # router.register('reviews', views.ReviewViewSet, basename='review')
 # router.register('payments', views.PaymentViewSet, basename='payment')
 
